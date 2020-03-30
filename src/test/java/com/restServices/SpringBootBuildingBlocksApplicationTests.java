070f979a80892ea0b3da054aa197b3a56e6b49e7
nnagwek <niket1409@gmail.com>
2020-03-30 14:19:24 +0530

    First Code Commit

---
+++ src/test/java/com/restServices/SpringBootBuildingBlocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBuildingBlocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
