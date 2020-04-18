cde13694ac14fa931fc7132a0a27be4ccf633287
nnagwek <niket1409@gmail.com>
2020-04-19 01:56:32 +0530

    08-SpringBoot-Internationalization

--- src/main/java/com/restServices/Hello/HelloWorldController.java
+++ src/main/java/com/restServices/Hello/HelloWorldController.java
@@ -1,6 +1,12 @@
 package com.restServices.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 //RestController
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	
 	//Simple method
 	//URI
@@ -26,4 +35,18 @@ public class HelloWorldController {
 		
 	}
 	
+	
+	@GetMapping("/hello-int")
+	public String getMessageIN18Format(@RequestHeader(name = "Accept-Language",required = false) String locale) {
+		
+		return messageSource.getMessage( "label.hello",null, new Locale(locale));
+		
+	}
+	
+	@GetMapping("/hello-int2")
+	public String getMessageIN18Format2( ) {
+		
+		return messageSource.getMessage( "label.hello",null , LocaleContextHolder.getLocale());
+		
+	}
 }
